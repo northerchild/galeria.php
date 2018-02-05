b9404e95a2ddd479403226ed72227ce7fa7907b5
Northerchild <osarus-13@hotmail.com>
2018-02-05 16:55:35 -0500

    Se crea el layout principal

---
+++ assets/views/indexView.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link href="https://fonts.googleapis.com/css?family=Slabo+27px" rel="stylesheet">
+	<link rel="stylesheet" href="assets/css/estilos.css">
+	<title>Galeria</title>
+</head>
+<body>
+	<header>
+		<div class="contenedor">
+			<h1 class="tituli">Mi Galeria</h1>
+		</div>
+	</header>
+	<section class="fotos">
+		<div class="contenedor">
+			<div class="thumb">
+				<a href="#">
+					<img src="assets/fotos/1.jpg" alt="">
+				</a>
+			</div>
+			<div class="thumb">
+				<a href="#">
+					<img src="assets/fotos/2.jpg" alt="">
+				</a>
+			</div>
+			<div class="thumb">
+				<a href="#">
+					<img src="assets/fotos/3.jpg" alt="">
+				</a>
+			</div>
+			<div class="thumb">
+				<a href="#">
+					<img src="assets/fotos/4.jpg" alt="">
+				</a>
+			</div>
+			<div class="thumb">
+				<a href="#">
+					<img src="assets/fotos/5.jpg" alt="">
+				</a>
+			</div>
+			<div class="thumb">
+				<a href="#">
+					<img src="assets/fotos/6.jpg" alt="">
+				</a>
+			</div>
+			<div class="thumb">
+				<a href="#">
+					<img src="assets/fotos/7.jpg" alt="">
+				</a>
+			</div>
+			<div class="thumb">
+				<a href="#">
+					<img src="assets/fotos/8.jpg" alt="">
+				</a>
+			</div>
+			<div class="paginacion">
+				<a href="#" class="izquierda"><i class="fa fa-long-arrow-left"></i>Pagina Anterior</a>
+				<a href="#" class="derecha">Pagina Siguiente <i class="fa fa-long-arrow-right"></i></a>
+			</div>
+		</div>
+	</section>
+	<footer>
+		<p class="copyright">Galeria creada por Northerchild &copy;</p>
+	</footer>
+</body>
+</html>
