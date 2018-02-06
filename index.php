de6f3a40b23e4417548cc4848722cff7efaa261a
Northerchild <osarus-13@hotmail.com>
2018-02-06 16:27:17 -0500

    Se crea la logica para la paginacion y numero de paginas

--- index.php
+++ index.php
@@ -1,4 +1,30 @@
 <?php
+	require_once "funciones.php";
+
+	$fotos_por_pagina = 8;
+	$pagina_actual = (isset($_GET['p']) ? (int)$_GET['p'] : 1);
+	$inicio = ($pagina_actual >1) ? ($pagina_actual * $fotos_por_pagina - $fotos_por_pagina): 0;
+
+	$conexion = conexion('galeria','root','');
+
+	if (!$conexion) {
+		die();
+	}
+
+	$statement = $conexion->prepare("SELECT SQL_CALC_FOUND_ROWS* FROM fotos LIMIT $inicio, $fotos_por_pagina");
+	$statement->execute();
+	$fotos = $statement->fetchAll();
+
+	if (!$fotos) {
+		header('Location: index.php');
+	}
+
+	$statement = $conexion->prepare("SELECT FOUND_ROWS() as total");
+	$statement->execute();
+	$total_post = $statement->fetch()['total'];
+
+	$total_pagina = ceil($total_post/ $fotos_por_pagina);
+	print_r($total_pagina);
 
 	require_once "/assets/views/indexView.php"
 
